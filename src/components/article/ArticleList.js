0cc64d3987cfac3255a783d31528a322e0e6bd8c
kantraman <93331484+kantraman@users.noreply.github.com>
2022-02-04 12:41:20 +0530

    Edit/ delete buttons added in Blog List

--- src/components/article/ArticleList.js
+++ src/components/article/ArticleList.js
@@ -12,8 +12,12 @@ const ArticleList = ({admin}) => {
     async function fetchAPI() {
         const response = await fetch(`/api/posts`);
         const body = await response.json();
-        console.log(body);
         setPosts(body);
+    }
+    const confirmDelete = (event) => {
+        const confirm = window.confirm("Delete the blog post?");
+        if (!confirm)
+            event.preventDefault();
     }
     return (
         <div>
@@ -21,9 +25,17 @@ const ArticleList = ({admin}) => {
             <Link className={admin ? "createBlog" : "hidden"} to="/post/create">Create Post</Link>
             {
                 posts.map((i, key) => (
-                    <Link className="articleList" key={key} to={`/article/${i._id}`}>
-                        {i.title}
-                    </Link>
+                    <div className="articleList" key={key}>
+                        <Link className="articleListItem"  to={`/article/${i._id}`}>
+                            {i.title}
+                        </Link>
+                        <Link className={admin ? "articleListItem" : "hidden"} to={`/post/update/${i._id}`}>
+                            ✍
+                        </Link>
+                        <Link onClick={confirmDelete} className={admin ? "articleListItem" : "hidden"} to={`/post/delete/${i._id}`}>
+                            🗑
+                        </Link>
+                    </div>
                 ))
             }
             
